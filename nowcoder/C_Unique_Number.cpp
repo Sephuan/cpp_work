f9682abfffbf6fea8ac231a3cea80e9ab1317c93
Sephuan <3113136832@qq.com>
2026-03-08 23:36:42 +0800

    贪心 思维

---
+++ nowcoder/C_Unique_Number.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    int ans = n;
+    rep1(i, 1, n) {
+        int d;
+        cin >> d;
+        ans = min(ans, d + i);
+    }
+    cout << ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
