d7c48a87561bb53b71f168e5d13481821e22752d
Sephuan <3113136832@qq.com>
2026-03-11 20:16:44 +0800

    按位异或 哈希

---
+++ nowcoder/C_小橙的完美序列.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    unordered_map<ll, int> um;
+    int cnt = 0;
+    rep1(i, 1, n) {
+        ll t;
+        cin >> t;
+        t ^= i;
+        um[t] ++;
+        if (um[t] > cnt) cnt = um[t];
+    }
+    cout << n - cnt;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
