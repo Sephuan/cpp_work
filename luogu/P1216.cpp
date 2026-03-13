2eeed8df3e3e999b1f2e9204085f5fbea721dcfc
Sephuan <3113136832@qq.com>
2026-03-13 18:52:33 +0800

    dp

---
+++ luogu/P1216.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+int dp[1'005][1'005];
+void solve() {
+    int r;
+    cin >> r;
+    int maxs = 0;
+    rep1(i, 1, r) {
+        rep1(j, 1, i) {
+            int t;
+            cin >> t;
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + t;
+            maxs = max(maxs, dp[i][j]);
+        }
+    }
+    cout << maxs;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
