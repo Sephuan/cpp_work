f976157e73c9006f90275e4e9c053c7379eca278
Sephuan <3113136832@qq.com>
2026-03-14 13:45:22 +0800

    卢卡斯定理 杨辉三角

---
+++ ctgu_oj/符文回溯.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, 1, -1};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n + 1);
+    rep1(i, 1, n) cin >> a[i];
+    while (q--) {
+        int p;
+        cin >> p;
+        if (((n - 1) & p - 1) == p - 1 && a[p])
+            cout << "YES";
+        else
+            cout << "NO";
+        if (q) cout << '\n';
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
