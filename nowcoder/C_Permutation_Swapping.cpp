e836831c50c24e7ccde9456149850084c01f88c4
Sephuan <3113136832@qq.com>
2026-03-16 21:15:05 +0800

    构造 分类讨论

---
+++ nowcoder/C_Permutation_Swapping.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, 1, -1};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n + 1);
+    rep1(i, 1, n) cin >> v[i];
+    if (n == 1) cout << "YES";
+    else if (n == 2 && v[1] < v[2]) cout << "YES";
+    else if (n == 3 && v[2] == 2) cout << "YES";
+    else if (n >= 4) cout << "YES";
+    else cout << "NO";
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
