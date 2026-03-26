608a34052b4ea35b92b0d6ce38b4b38b9003515d
Sephuan <3113136832@qq.com>
2026-03-26 19:44:40 +0800

    思维 数学

---
+++ Atcoder/D_Minimize_Range.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int N, K;
+    cin >> N >> K;
+    vector A(N, 0);
+    rep(i, 0, N) {
+        int t;
+        cin >> t;
+        A[i] = t % K;
+    }
+    sort(A.begin(), A.end());
+    int dis = K - A.back() + A.front();
+    rep(i, 1, N) {
+        dis = max(dis, A[i] - A[i - 1]);
+    }
+    cout << K - dis;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
