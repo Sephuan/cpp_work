cd2bd03b31a619df5e75c1a787f3d6cfd9aa1205
Sephuan <3113136832@qq.com>
2026-04-03 21:14:31 +0800

    递归 字符串 黄

---
+++ luogu/P1928_外星密码.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+string read() {
+    string res = "";
+    char c;
+    while (cin >> c) {
+        if (c == '[') {
+            int n;
+            cin >> n;
+            string s = read();
+            while (n --) res += s; 
+        } else if (c == ']') {
+            return res;
+        } else res += c;
+    }
+    return res;
+}
+
+void solve() {
+    cout << read();
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
