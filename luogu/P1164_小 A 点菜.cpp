9da9d1db727343e86f007d38f3aa44caae124f4e
Sephuan <3113136832@qq.com>
2026-04-04 15:24:06 +0800

    dp 01背包 橙

---
+++ luogu/P1164_小 A 点菜.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> a(N + 1);
+    rep1(i, 1, N) cin >> a[i];
+    vector<int> dp(M + 1, 0);
+    dp[0] = 1;
+    rep1(i, 1, N) {
+        int p = a[i];
+        rrep(j, M, p)
+            dp[j] += dp[j - p];
+    }
+    cout << dp[M];
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
