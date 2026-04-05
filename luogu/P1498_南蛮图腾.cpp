6db3dc914239e7dbe00ac164ef074006d6607784
Sephuan <3113136832@qq.com>
2026-04-05 17:15:16 +0800

    字符串 分形 模拟 黄

---
+++ luogu/P1498_南蛮图腾.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+/*
+       /\
+      /__\
+     /\  /\
+    /__\/__\
+   /\      /\
+  /__\    /__\
+ /\  /\  /\  /\
+/__\/__\/__\/__\
+*/
+void solve() {
+    int n;
+    cin >> n;
+    int th = 1 << n;
+    vector<string> a(th);
+    a[0] = " /\\ ";
+    a[1] = "/__\\";
+    int h = 2;
+    while (h < th) {
+        rep(i, 0, h) {
+            a[i + h] = a[i] + a[i];
+            a[i] = string(h, ' ') + a[i] + string(h, ' ');
+        }
+        h <<= 1;
+    }
+    rep(i, 0, th) cout << a[i] << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
