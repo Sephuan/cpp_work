0f249901ae5843fb17bd802596a569d5e9da0038
Sephuan <3113136832@qq.com>
2026-04-18 22:08:39 +0800

    模板

---
+++ Atcoder/1.py
@@ -0,0 +1,10 @@
+import sys
+from collections import *
+
+def solve():
+    d = sys.stdin.read().splitlines();
+    ans = []
+    sys.stdout.write('\n'.join(ans))
+
+if __name__ == '__main__':
+    solve()
