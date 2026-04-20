6ca32bcbd3a28806008513ef06487f12bdd10058
Sephuan <3113136832@qq.com>
2026-04-20 19:25:57 +0800

    构造 数学

---
+++ nowcoder/F_小红的三角形构造.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    ll x;
+    cin >> x;
+    if (x <= 2) cout << "No";
+    else if (x & 1) {
+        ll b = x * x - 1 >> 1;
+        cout << "Yes" << endl;
+        cout << x << ' ' << b << ' ' << b + 1; 
+    } else {
+        ll b = x * x - 4 >> 2;
+        cout << "Yes" << endl;
+        cout << x << ' ' << b << ' ' << b + 2;
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
