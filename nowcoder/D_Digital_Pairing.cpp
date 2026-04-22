7d4ef0f319bd32e27265517c3f3dfcac4713e156
Sephuan <3113136832@qq.com>
2026-04-22 20:30:32 +0800

    贪心 位运算

---
+++ nowcoder/D_Digital_Pairing.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<ll> a(n << 1);
+    rep(i, 0, n << 1) cin >> a[i];
+    ll ans = 0;
+    rrep(i, 40, 0) {
+        ll t = ans | (1LL << i);
+        int cnt = 0;
+        for (auto& x : a) {
+            if ((x & t) == t)
+                cnt ++;
+        }
+        if (cnt >= n) ans = t;
+    }
+    cout << ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
