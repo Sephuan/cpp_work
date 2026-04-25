8c0eb84ba56182e7bc436a05f0795fec5b8972dd
Sephuan <3113136832@qq.com>
2026-04-25 10:52:32 +0800

    构造

---
+++ nowcoder/F_小彩的好数构造.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+bool f(int x) {
+    set<int> t = {1, 2, 3};
+    int lst = -1;
+    set<int> s;
+    while (x) {
+        int r = x % 10;
+        if (r == lst) return false;
+        lst = r;
+        s.insert(x % 10);
+        x /= 10;
+    }
+    return t == s;
+}
+
+void solve() {
+    int n; cin >> n;
+    if (n == 1) {
+        cout << -1;
+        return ;
+    }
+    if (n & 1) {
+        cout << 1;
+        cout << string(n - 2, '0');
+        cout << 1 << ' ';
+        rep(i, 0, n - 1 >> 1) cout << "13";
+        cout << 1;
+    } else {
+        cout << 1;
+        cout << string(n - 2, '0');
+        cout << 1 << ' ';
+        rep(i, 0, n >> 1) cout << "12";
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
