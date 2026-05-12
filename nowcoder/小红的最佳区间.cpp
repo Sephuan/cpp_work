cee172c1da925bbd1c05a76ddd40d23c9b3b83ae
Sephuan <3113136832@qq.com>
2026-05-12 21:15:00 +0800

    扫描线

---
+++ nowcoder/小红的最佳区间.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<pii> vp;
+    rep1(i, 1, n) {
+        int l, r; cin >> l >> r;
+        vp.push_back({l - k, 1});
+        vp.push_back({r, 0});
+    }
+    sort(vp.begin(), vp.end(), [](auto& a, auto& b) {
+        if (a.fi != b.fi) return a.fi < b.fi;
+        return a.se > b.se;
+    });
+    int cnt = 0;
+    int ans = 0;
+    for (auto& [i, f] : vp) {
+        if (f) {
+            cnt ++;
+            ans = max(ans, cnt);
+        } else cnt --;
+    }
+    cout << ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
