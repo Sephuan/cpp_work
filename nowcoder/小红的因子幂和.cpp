f4854f019082046a41703b80c8d03a407149eb7e
Sephuan <3113136832@qq.com>
2026-05-12 20:35:00 +0800

    质因数分解 dfs

---
+++ nowcoder/小红的因子幂和.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+ll qpow(ll a, ll b) {
+    a %= mod;
+    ll r = 1;
+    while (b) {
+        if (b & 1) r = r * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return r;
+}
+
+void solve() {
+    ll x, y; cin >> x >> y;
+    map<ll, int> fmp;
+    auto dfp = [&](int t) {
+        for (int i = 2; 1LL * i * i <= t; ++i) {
+            while (t % i == 0) {
+                fmp[i] ++;
+                t /= i;
+            }
+        }
+        if (t > 1) fmp[t] ++;
+    };
+    dfp(x); dfp(y);
+    vector<pair<ll, int>> facs;
+    for (auto& item : fmp) facs.push_back(item);
+    ll ans = 0;
+    auto dfs = [&](auto&& self, int u, ll cur) {
+        if (u == facs.size()) {
+            ans = (ans + qpow(cur, cur)) % mod;
+            return ;
+        }
+        ll p = facs[u].fi; int count = facs[u].se;
+        ll tmp = cur;
+        rep1(i, 0, count) {
+            self(self, u + 1, tmp);
+            if (i < count) tmp *= p;
+        }
+    };
+    dfs(dfs, 0, 1);
+    cout << ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
