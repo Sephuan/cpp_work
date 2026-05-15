4e31b14bde05d863997725f96cd480e7064d526e
Sephuan <3113136832@qq.com>
2026-05-15 19:13:53 +0800

    01背包 橙

---
+++ luogu/P1048_采药.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int T, M; cin >> T >> M;
+    int dp[1'005];
+    memset(dp, 0, sizeof(dp));
+    vector<pii> a;
+    rep(i, 0, M) {
+        int t, v; cin >> t >> v;
+        a.push_back({t, v});
+    }
+    rep(i, 0, M) rrep(j, T, 0) {
+        if (j >= a[i].fi)
+        dp[j] = max(dp[j], dp[j - a[i].fi] + a[i].se);
+    }
+    cout << dp[T];
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
