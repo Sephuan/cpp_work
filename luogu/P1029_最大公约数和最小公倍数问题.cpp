82e05e5dd1f434bcf5c1e46aa256459819e90a8a
Sephuan <3113136832@qq.com>
2026-05-15 18:58:32 +0800

    枚举 橙

---
+++ luogu/P1029_最大公约数和最小公倍数问题.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int x, y; cin >> x >> y;
+    int ans = 0;
+    int mul = x * y;
+    for (int i = 1; i * i <= mul; ++i) {
+        if (mul % i == 0) {
+            if (__gcd(i, mul / i) == x) {
+                if (i != mul / i) ans += 2;
+                else ans += 1;
+            }
+        }
+    }
+    cout << ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
