89aa34f52423fc29788707ae089fe673413f6773
Sephuan <3113136832@qq.com>
2026-05-15 20:14:33 +0800

    二分答案 橙

---
+++ luogu/P1873_砍树.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int N, M; cin >> N >> M;
+    vector<ll> a(N + 1);
+    rep1(i, 1, N) cin >> a[i];
+    sort(a.begin() + 1, a.end());
+    auto chk = [&](int k) {
+        ll sum = 0;
+        rep1(i, 1, N) sum += max(0LL, a[i] - k);
+        return sum >= M;
+    };
+    int l = 0, r = 2e9;
+    int ans = 0;
+    while (l <= r) {
+        int mid = l + (r - l >> 1);
+        if (chk(mid)) {
+            ans = max(ans, mid);
+            l = mid + 1;
+        } else r = mid - 1;
+    }
+    cout << ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
