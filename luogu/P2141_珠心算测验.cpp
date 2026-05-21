63d50cc9ad68883142888042e000c4c36657f83a
Sephuan <3113136832@qq.com>
2026-05-21 16:57:18 +0800

    枚举 模拟 橙

---
+++ luogu/P2141_珠心算测验.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'05;
+
+void init() {
+
+}
+
+void solve() {
+    int n; cin >> n;
+    vector<int> mp(MAXN), a(n);
+    vector<int> counted(MAXN);
+    rep(i, 0, n) {
+        cin >> a[i];
+        mp[a[i]] = 1;
+    }
+    int ans = 0;
+    rep(i, 0, a.size() - 1) rep(j, i + 1, a.size()) {
+        int sum = a[i] + a[j];
+        if (mp[sum] && !counted[sum]) {
+            ans ++;
+            counted[sum] = 1;
+        }
+    }
+    cout << ans;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
