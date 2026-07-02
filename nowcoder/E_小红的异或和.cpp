29af2d7629c488b9d9bd130eec02bf7236783374
Sephuan <3113136832@qq.com>
2026-07-02 16:22:22 +0800

    位运算

---
+++ nowcoder/E_小红的异或和.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 1'000'005;
+
+void init() {
+
+}
+
+ll f(ll n) {
+    ll m = n % 4;
+    if (m == 0) return n;
+    else if (m == 1) return 1LL;
+    else if (m == 2) return n + 1;
+    return 0LL;
+}
+
+void solve() {
+    ll l, r, x;
+    cin >> l >> r >> x;
+    ll mina = (l + x - 1) / x;
+    ll maxa = r / x;
+    if (mina > maxa) {
+        cout << 0;
+        return ;
+    }
+    cout << (f(maxa) ^ f(mina - 1)) * x;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
