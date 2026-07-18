70b028b6fcdc128ccf282416fcd37a602081ae95
Sephuan <3113136832@qq.com>
2026-07-18 16:08:09 +0800

    数学 分治

---
+++ Atcoder/abc448_e_Simple_Division.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 1'000'005;
+
+
+void init() {
+
+}
+
+void solve() {
+    ll K, M; cin >> K >> M;
+    ll Mod = 10'007LL * M;
+    auto powerAndSum = [&](auto&& self, ll n) -> pll {
+        if (n == 0) return {1, 0};
+        auto [p, s] = self(self, n >> 1);
+        ll p2 = p * p % Mod;
+        ll s2 = (s + p * s) % Mod;
+        if (n & 1) return {p2 * 10 % Mod, (s2 + p2) % Mod};
+        return {p2, s2};
+    };
+    ll rem = 0;
+    rep(i, 0, K) {
+        ll c, l; cin >> c >> l;
+        auto [power, sum] = powerAndSum(powerAndSum, l);
+        rem = (rem * power % Mod + c * sum % Mod) % Mod;
+    }
+    cout << rem / M;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
