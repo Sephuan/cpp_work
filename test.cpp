d58cc7f2138ea355247905bcb8355a2b6340cca5
Sephuan <3113136832@qq.com>
2026-07-27 20:26:49 +0800

    temp

--- test.cpp
+++ test.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define fi first
 #define se second
 #define pii pair<int, int>
+#define pll pair<long long, long long>
 #define endl '\n'
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
 #define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
@@ -13,10 +14,13 @@ using namespace std;
 //#define int long long
 //#define int unsigned long long
 constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
 constexpr int mod = 1e9+7;
 constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3fLL;
 constexpr int dx[] = {-1, 1, 0, 0};
-constexpr int dy[] = {0, 0, 1, -1};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
 
 const double PI = acos(-1.0);
 const int MAXN = 2'00'005;
