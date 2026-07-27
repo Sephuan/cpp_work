eefaeebc2c46172317afc2436e023f765c703a63
Sephuan <3113136832@qq.com>
2026-07-27 15:00:55 +0800

    排序 + 有序组合生成 + 状态压缩 + 大根堆 + Best-First Search + 懒生成前 X 大 + 单调扩展去重

---
+++ Atcoder/abc440_e_Cookies.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define AC return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3f;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 2'00'005;
+
+void init() {
+
+}
+
+void solve() {
+    int N, K, X; cin >> N >> K >> X;
+    vector<ll> A(N); rep(i, 0, N) cin >> A[i];
+    ranges::sort(A, greater<>());
+    priority_queue<tuple<ll, int, int>> pq;
+    pq.push({A[0] * K, 0, 1});
+    rep(rp, 0, X) {
+        auto [S, c, pos] = pq.top(); pq.pop();
+        cout << S << endl;
+        if (c < K) rep(i, pos, N) {
+            pq.push({S - A[0] + A[i], c + 1, i});
+        }
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    //cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    AC
+}
