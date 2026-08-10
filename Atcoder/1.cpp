b88a1072b21ceb1a6213dffac703b9b8b85f28b7
Sephuan <3113136832@qq.com>
2026-08-10 15:04:17 +0800

    temp

--- Atcoder/1.cpp
+++ Atcoder/1.cpp
@@ -10,7 +10,7 @@ using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); ++i)
 #define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
 #define rrep(i, a, b) for (int i = (a); i >= (b); --i)
-#define AC return 0;
+#define Sephuan return 0;
 //#define int long long
 //#define int unsigned long long
 constexpr int MOD = 998'244'353;
@@ -23,7 +23,7 @@ constexpr int dy[] = {0, 0, -1, 1};
 constexpr char dc[] = {'U', 'D', 'L', 'R'};
 
 const double PI = acos(-1.0);
-const int MAXN = 2'00'005;
+const int MAXN = 10'000'005;
 
 void init() {
 
@@ -39,12 +39,12 @@ signed main() {
     init();
     int T = 1;
     //cin >> T;
-    cout << fixed << setprecision(15);
+    //cout << fixed << setprecision(15);
     while (T--) {
         solve();
         if (T) {
             cout << '\n';
         }
     }
-    AC
+    Sephuan
 }
