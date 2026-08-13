67a8c50d5870cb9f5e94fc1ae324024735521da8
Sephuan <3113136832@qq.com>
2026-08-13 15:35:25 +0800

    博弈论  贪心 计数 DP  排序  括号序列 卡特兰数

---
+++ nowcoder/B_Deep_Finesse.cpp
@@ -0,0 +1,135 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using ull = unsigned long long;
+using namespace std;
+#define fi first
+#define se second
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define endl '\n'
+#define rep(i, a, b) for (int i = (a); i < (b); ++i)
+#define rep1(i, a, b) for (int i = (a); i <= (b); ++i)
+#define rrep(i, a, b) for (int i = (a); i >= (b); --i)
+#define Sephuan return 0;
+//#define int long long
+//#define int unsigned long long
+constexpr int MOD = 998'244'353;
+constexpr int MOD_P = MOD - 1;
+constexpr int mod = 1e9+7;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f'3f3f3f3fLL;
+constexpr int dx[] = {-1, 1, 0, 0};
+constexpr int dy[] = {0, 0, -1, 1};
+constexpr char dc[] = {'U', 'D', 'L', 'R'};
+
+const double PI = acos(-1.0);
+const int MAXN = 200'005;
+
+template <int MOD>
+struct ModInt {
+    int v;
+
+    ModInt() : v(0) {}
+    ModInt(long long _v) { 
+        v = _v % MOD; 
+        if (v < 0) v += MOD; 
+    }
+
+    ModInt power(long long p) const {
+        ModInt res = 1, base = *this;
+        while (p > 0) {
+            if (p & 1) res *= base;
+            base *= base;
+            p >>= 1;
+        }
+        return res;
+    }
+
+    ModInt inv() const { return power(MOD - 2); }
+
+    ModInt& operator+=(const ModInt& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    ModInt& operator-=(const ModInt& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    ModInt& operator*=(const ModInt& o) { v = 1LL * v * o.v % MOD; return *this; }
+    ModInt& operator/=(const ModInt& o) { return *this *= o.inv(); } 
+
+    friend ModInt operator+(ModInt a, const ModInt& b) { return a += b; }
+    friend ModInt operator-(ModInt a, const ModInt& b) { return a -= b; }
+    friend ModInt operator*(ModInt a, const ModInt& b) { return a *= b; }
+    friend ModInt operator/(ModInt a, const ModInt& b) { return a /= b; }
+
+    bool operator==(const ModInt& o) const { return v == o.v; }
+    bool operator!=(const ModInt& o) const { return v != o.v; }
+
+    friend istream& operator>>(istream& is, ModInt& x) {
+        long long val; is >> val; x = ModInt(val); return is;
+    }
+    friend ostream& operator<<(ostream& os, const ModInt& x) {
+        return os << x.v;
+    }
+};
+
+using MI_1e9 = ModInt<1000000007>;
+using MI_998 = ModInt<998244353>;
+using MI = MI_998;
+
+void init() {
+
+}
+
+struct DSU {
+    vector<int> p, sz;
+    DSU(int n) : p(n), sz(n) {
+        iota(p.begin(), p.end(), 0);
+    }
+    int find(int x) {
+        return p[x] == x ? x : p[x] = find(p[x]);
+    }
+    void unite(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) return ;
+        if (sz[u] < sz[v]) swap(u, v);
+        sz[u] += sz[v];
+        p[v] = p[u];
+    }
+};
+
+void solve() {
+    int n, m; cin >> n >> m;
+    vector<int> used((n << 1) + 1);
+    bool ok = 1;
+    rep(i, 0, m) {
+        int a; cin >> a;
+        if (a >= (n << 1)) {
+            ok = 0;
+        } else {
+            used[a] = 1;
+        }
+    }
+    if (!ok) {
+        cout << 0;
+        return ;
+    }
+    vector dp((n << 1) + 1, vector<MI>(n + 1, 0));
+    dp[0][0] = 1;
+    rep1(i, 1, n << 1) rep1(j, 0, n) {
+        if (j < n) dp[i][j + 1] += dp[i - 1][j];
+        if (!used[i] && i - j <= j) dp[i][j] += dp[i - 1][j];
+    }
+    cout << dp[n << 1][n];
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    init();
+    int T = 1;
+    cin >> T;
+    cout << fixed << setprecision(15);
+    while (T--) {
+        solve();
+        if (T) {
+            cout << '\n';
+        }
+    }
+    Sephuan
+}
